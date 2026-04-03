5cc6053611e0e5955f919f1f80a6b012498ab219
Tatiana Ivanova <s33285@pjwstk.edu.pl>
2026-04-03 10:31:15 +0200

    Added adpater to wrap LegacyBillingGateway

---
+++ LegacyRenewalApp/Helpers/BillingGatewayAdapter.cs
@@ -0,0 +1,16 @@
+﻿using LegacyRenewalApp.Interfaces;
+
+namespace LegacyRenewalApp.Helpers;
+
+public class BillingGatewayAdapter : IBillingGateway
+{
+    public void SaveInvoice(RenewalInvoice invoice)
+    {
+        LegacyBillingGateway.SaveInvoice(invoice);
+    }
+
+    public void SendEmail(string email, string subject, string body)
+    {
+        LegacyBillingGateway.SendEmail(email, subject, body);
+    }
+}
